3c0411a927fb1b560eb3e89cf1bcab776abf9dfe
Himanshu Garg <himanshu.g@zebpay.com>
2022-06-13 02:10:03 +0530

    Url fix

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const rp = require('request-promise');
+const axios = require('axios');
+
+
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+//   const nameToGreet = core.getInput('who-to-greet');
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = (new Date()).toTimeString();
+//   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+//   const payload = JSON.stringify(github.context.payload, undefined, 2)
   const options = {
     method: 'POST',
     uri: 'https://hooks.slack.com/services/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',
@@ -20,20 +23,36 @@ try {
     };
     slackmessage()
     
-  console.log(`The event payload: ${payload}`);
+//   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
 
-const slackmessage = async () => {
-    const options = {
-        method: 'POST',
-        uri: 'https://hooks.slack.com/services/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',
-        body: {
-            text:"Hello, World!"
-        },
-        json: true // Automatically stringifies the body to JSON
-        };
-        const res = await rp(options)
-        return res;
+async function slackmessage() {
+    try {
+        await axios({
+            method: 'POST',
+            url: 'https://hooks.slack.com/services/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',
+            data: {
+              channel: 'github-prs',
+              username: 'Pull Request reviews reminder',
+              text: "Hello, World!",
+            },
+          });
+
+    }catch(err) {
+        console.log("err----",err)
+    }
+   
+    // const options = {
+    //     method: 'POST',
+    //     uri: 'https://hooks.slack.com/services/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',
+    //     body: {
+    //         text:"Hello, World!"
+    //     },
+    //     json: true // Automatically stringifies the body to JSON
+    //     };
+    //     const res = await rp(options)
+    //     console.log(res.statusCode)
+        // return res;
 }
