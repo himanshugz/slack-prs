62b01f8cff1c20840a62f5394c87ff6ffb0cf9cb
Himanshu Garg <himanshu.g@zebpay.com>
2022-06-13 09:08:07 +0530

    Add vars

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ const axios = require('axios');
 
 try {
   // `who-to-greet` input defined in action metadata file
-//   const nameToGreet = core.getInput('who-to-greet');
+  const webhookUrl = core.getInput('webhook_url');
+  const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_API_URL } = process.env;
+  console.log(GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_API_URL)
 //   console.log(`Hello ${nameToGreet}!`);
 //   const time = (new Date()).toTimeString();
 //   core.setOutput("time", time);
@@ -15,30 +17,22 @@ try {
 //   const payload = JSON.stringify(github.context.payload, undefined, 2)
   const options = {
     method: 'POST',
-    uri: 'https://hooks.slack.com/services/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',
+    uri: webhookUrl,
     body: {
         text:"Hello, World!"
     },
     json: true // Automatically stringifies the body to JSON
     };
-    slackmessage()
+    slackmessage(options)
     
 //   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
 
-async function slackmessage() {
+async function slackmessage(options) {
     try {
-        await axios({
-            method: 'POST',
-            url: 'https://hooks.slack.com/services/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',
-            data: {
-              channel: 'github-prs',
-              username: 'Pull Request reviews reminder',
-              text: "Hello, World!",
-            },
-          });
+        await axios(options);
 
     }catch(err) {
         console.log("err----",err)
